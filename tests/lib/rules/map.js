34c3139575a04f0772304ce26839a19e911b3b48
Wansmer <speakmed@gmail.com>
2020-03-20 00:02:26 +0300

    add test

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -1,24 +1,57 @@
-const rule = require("../../../lib/rules/my-rule"),
-    RuleTester = require("eslint").RuleTester;
+"use strict";
 
-const ruleTester = new RuleTester();
+const rule = require("../../../lib/rules/map"),
+  RuleTester = require("eslint").RuleTester;
 
-ruleTester.run("my-rule", rule, {
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6, parser: "babel-eslint" } });
+
+ruleTester.run("lodash-to-native/map", rule, {
   valid: [
-      {
-          code: "var foo = true",
-          options: [{ allowFoo: true }]
-      }
+    // don't show error if collection - object
+    {
+      code: "_.map({1: 2, 2: 2}, (x) => x);"
+    },
+    {
+      code: "const obj = {1: 2, 2: 2}; _.map({1: 2, 2: 2}, (x) => x);"
+    },
+    {
+      code: "let obj = [1, 2]; obj = {1: 2, 2: 2}; _.map({1: 2, 2: 2}, (x) => x);"
+    },
+    // don't show error if collection argument of function
+    {
+      code: "function workCollection (arg) {_.map(arg, (x) => x);}"
+    },
+    {
+      code: "const workCollection = (arg) => {_.map(arg, (x) => x);}"
+    },
   ],
-
   invalid: [
-      {
-          code: "var invalidVariable = true",
-          errors: [{ message: "Unexpected invalid variable." }]
-      },
-      {
-          code: "var invalidVariable = true",
-          errors: [{ message: /^Unexpected.+variable/ }]
-      }
+    // show error and suggestion to fix
+    {
+      code: "const arr = [1, 2]; _.map(arr, (x) => x);",
+      errors: [{ 
+        message: "change _.map by native, if possible. Fix it auto with eslint."
+      }]
+    },
+    {
+      code: "_.map([1, 2], (x) => x);",
+      errors: [{ 
+        message: "change _.map by native, if possible. Fix it auto with eslint."
+      }]
+    },
+    {
+      code: `let arr = {1: 2, 2: 2}; 
+                  arr = [1, 2]; 
+                  _.map(arr, (x) => x);`,
+      errors: [{ 
+        message: "change _.map by native, if possible. Fix it auto with eslint."
+      }]
+    },
+    {
+      code: "_.map(new Array(), (x) => x);",
+      errors: [{ 
+        message: "change _.map by native, if possible. Fix it auto with eslint."
+      }]
+    },
   ]
 });
